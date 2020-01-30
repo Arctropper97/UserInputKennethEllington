eaa52840a2c4920d17371bf50d8fb87b646bd369
Kenneth Ellington <kellington1@usf.edu>
2020-01-30 17:00:44 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UserInputKennethEllington
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string starWars = "";
+            Console.WriteLine("Hello this is Kenneth Ellington");
+
+            Console.WriteLine("Plese enter in your favorite star wars charcater. \n " +
+                "After your finished please hit the enter sign button.");
+            starWars = Console.ReadLine();
+
+            Console.WriteLine("Your favorite chacter is " + starWars);
+
+
+        }
+    }
+}
